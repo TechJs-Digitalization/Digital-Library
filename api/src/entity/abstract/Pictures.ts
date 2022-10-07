7b9470262590b804a188ef511031b8de72a72954
Tolotra <tolotramandresy39@gmail.com>
2022-10-07 15:37:13 +0300

    Deleted multi-pictures option from author

--- api/src/entity/abstract/Pictures.ts
+++
@@ -1,9 +0,0 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
-
-export abstract class Picture{
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column({unique: true})
-    fileName: string;
-}
